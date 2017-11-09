3ada0c72f39f276cf2c45178040b506ec51677f8
aitoehigie <aitoehigie@gmail.com>
2017-11-09 01:11:58 +0100

    updates

--- src/helloworld/main.go
+++ src/helloworld/main.go
@@ -7,8 +7,9 @@ import (
 var (
 	message = "The answer to life is %d\n"
 	answer  = 42
+	pi      = 3.14
 )
 
 func main() {
-	fmt.Printf(message, answer)
+	fmt.Printf(message, answer, "while pi is", pi)
 }
